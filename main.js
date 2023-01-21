635f8897e1d32beff73380e3ac58ec143f76c1c1
tllwtg <tllwtg@gmail.com>
2023-01-21 10:58:08 +0800

    Support Windows

--- main.js
+++ main.js
@@ -2,6 +2,10 @@
 const { app, BrowserWindow } = require("electron");
 const path = require("path");
 
+// run this as early in the main process as possible
+// to Handling startup events of electron-winstaller
+if (require("electron-squirrel-startup")) app.quit();
+
 function createWindow() {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
